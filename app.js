5db714927d55699dcb1295b441639e9a47594a2e
Zack <zacharyfons@gmail.com>
2018-04-19 23:57:42 -0400

    Update script to change players and display winner

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ document.getElementById('current-1').textContent = '0';
 
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
-
 	// random number
 	var dice = Math.floor(Math.random() * 6) + 1;
 
@@ -42,14 +41,36 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 		document.querySelector('#current-' + activePlayer).textContent = roundScore;
 	} else {
 		// next player
-		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-		roundScore = 0;
-		document.getElementById('current-0').textContent = '0';
-		document.getElementById('current-1').textContent = '0';
-		document.querySelector('.player-0-panel').classList.toggle('active');
-		document.querySelector('.player-1-panel').classList.toggle('active');
+		nextPlayer();
 	}
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+	// Add current score to global score
+	scores[activePlayer] += roundScore;
+
+	// Update the UI
+	document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+	// Check if player is victorious and display the accolade
+	if (scores[activePlayer] >= 10) {
+		document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+		document.querySelector('.dice').style.display = 'none';
+		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+	} else {
+		nextPlayer();
+	}
+});
+
+function nextPlayer() {
+	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+	roundScore = 0;
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+	document.querySelector('.player-0-panel').classList.toggle('active');
+	document.querySelector('.player-1-panel').classList.toggle('active');
+}
 
 // var x = document.querySelector('#score-0').textContent;
 // document.querySelector('#current-').textContent = dice;
